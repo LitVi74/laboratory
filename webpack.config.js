acd1b1ed1af5421a0bbcddef0c135262726fb1fd
LitVi74 <nastya777mihalik@gmail.com>
2022-08-08 19:26:47 +0500

    edit scripts for windows

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 const path = require('path');
 const htmlWebpackPlugin = require("html-webpack-plugin");
-const mode = process.env.NODE_ENV;
 
 module.exports = {
-  mode,
   entry: path.resolve(__dirname, 'src') + '/index.ts',
   output: {
     path: path.resolve(__dirname, "./dist"),
