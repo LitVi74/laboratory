bc8678cd53e9fb0a662bfd5bce16d97415feefb6
LitVi74 <nastya777mihalik@gmail.com>
2022-08-16 12:51:23 +0500

    actions.ts: view serialize result

--- src/actions.ts
+++ src/actions.ts
@@ -3,13 +3,33 @@ import { checkValidly } from "./validator";
 
 const handleSubmitSerializeForm = (event: SubmitEvent) => {
   event.preventDefault();
-  const form = event.currentTarget as HTMLFormElement;
-  const numList = form.numList.value as string
 
-  const validlyResult = checkValidly(numList);
+  const form = event.currentTarget as HTMLFormElement;
+  const numList = form.getElementsByTagName('input').item(0);
+  let div = document.getElementById('serialize-error-text');
+
+  const validlyResult = checkValidly(numList.value, ["isNotNumber", "isTooBig"]);
   if (validlyResult.check) {
-     const numArray = numList.split(",", 200).map((string) => +string.trim());
-     console.log(serialize(numArray));
+    if (numList.classList.contains('input-error')){
+      numList.classList.remove('input-error')
+      form.removeChild(div)
+    }
+
+    const textBlock = form.getElementsByTagName('p').item(0);
+
+    const numArray = numList.value.split(",", 200).map((string) => +string.trim());
+    textBlock.textContent = serialize(numArray);
+  }else{
+    if (!div) {
+      div = document.createElement('div');
+      div.classList.add('error-text');
+      div.id = 'serialize-error-text';
+    }
+
+    div.textContent = validlyResult.error;
+
+    numList.classList.add('input-error');
+    form.insertBefore(div, numList.nextSibling)
   }
 };
 
